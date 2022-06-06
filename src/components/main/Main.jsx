f1d510c4385f735b10bb996a24e6253bbe96930e
Yulia Novakovskaya <yonovakovskaya@gmail.com>
2022-06-06 23:20:27 +0300

    Added Library page

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,8 +1,10 @@
+import LibraryPage from 'pages/LibraryPage';
 import TrainingPage from 'pages/TrainingPage';
 
 const Main = () => {
   return (
     <main>
+      <LibraryPage/>
       <TrainingPage />
     </main>
   );
