72151e7e5a1a5fbdb679cec36f001a31f57851d7
Artem <lazarazuy@gmail.com>
2022-06-11 20:40:54 +0300

    +

--- src/components/buttonStartTrenny/ButtonStartTrenny.jsx
+++ src/components/buttonStartTrenny/ButtonStartTrenny.jsx
@@ -1,18 +1,10 @@
 import React, { useState } from 'react';
 import { Button, BoxButton } from './CSSButtonTrenny';
 
-const StartTrenny = () => {
-  const [state, setState] = useState({
-    isActive: false,
-    data: [],
-  });
-  const { isActive } = state;
-  const createActive = () => {
-    setState({ ...state, isActive: true });
-  };
+const StartTrenny = ({ props }) => {
   return (
     <BoxButton>
-      <Button type="button" onClick={createActive}>
+      <Button type="button" onClick={props}>
         Почати тренування
       </Button>
     </BoxButton>
