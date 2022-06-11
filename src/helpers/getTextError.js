b09cc39ab438892334316617746e31857866267d
tatiana-borisova <0102tb@gmail.com>
2022-06-11 22:13:23 +0200

    Added default error

--- src/helpers/getTextError.js
+++ src/helpers/getTextError.js
@@ -1,9 +1,9 @@
-const statusSignupMapping = {
+const signupErrorMessages = {
   400: 'Здається, ви щось запровадили неправильно. Спробуйте ще раз.',
   409: 'Користувач з такою електронною поштою вже зареєстрований.',
 };
 
-const statusLoginMapping = {
+const loginErrorMessages = {
   403: 'Ви ввели неправильний логін або пароль.',
   404: 'Такий користувач не зареєстровано.',
   412: 'Ви не підтвердили реєстрацію. Будь ласка, перевірте пошту в папці вхідні або папку спам.',
@@ -12,5 +12,5 @@ const statusLoginMapping = {
 const getTextError = errors => status =>
   errors[status] || 'Упс, щось пішло не так, спробуйте пізніше повторити :)';
 
-export const getSignupError = getTextError(statusSignupMapping);
-export const getLoginError = getTextError(statusLoginMapping);
+export const getSignupError = getTextError(signupErrorMessages);
+export const getLoginError = getTextError(loginErrorMessages);
