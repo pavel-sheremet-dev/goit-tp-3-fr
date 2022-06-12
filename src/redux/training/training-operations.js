5e063d0d1058a1ac7338518278789bf44ccb0037
Yulia Novakovskaya <yonovakovskaya@gmail.com>
2022-06-12 22:33:29 +0300

    Added redux-trainings

--- src/redux/training/training-operations.js
+++ src/redux/training/training-operations.js
@@ -1,14 +1,50 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-export const addSelectBook = createAsyncThunk(
-  'training/addSelectBook',
-  async (book, { rejectWithValue }) => {
+const ADD_TRAINING_ENDPOINT = '/api/trainings';
+const GET_ACTIVE_TRAINING_ENDPOINT = '/api/trainings/';
+const UPDATE_ACTIVE_TRAINING_ENDPOINT = '/api/trainings//';
+
+export const addTraining = createAsyncThunk(
+  'training/addTraining',
+  async ({ startDate, deadlineDate, books: book }, { rejectWithValue }) => {
+    const books = book.map(({ id, pages }) => ({ id, pages }));
     try {
-      const { data } = await axios.post('', book);
+      const { data } = await axios.post(ADD_TRAINING_ENDPOINT, {
+        startDate,
+        deadlineDate,
+        books,
+      });
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  },
+);
+
+export const getActiveTraining = createAsyncThunk(
+  'training/getActiveTraining',
+  async (_, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.get(GET_ACTIVE_TRAINING_ENDPOINT);
       return data;
     } catch (error) {
       return rejectWithValue(error);
+    }
+  },
+);
+
+export const updateActiveTraining = createAsyncThunk(
+  'training/updateActiveTraining',
+  async ({ date, pointResult }, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.patch(UPDATE_ACTIVE_TRAINING_ENDPOINT, {
+        date,
+        pointResult,
+      });
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
     }
   },
 );
