fba21004a1362ce8c2f091f67ed10f66f0c899be
Yulia Novakovskaya <yonovakovskaya@gmail.com>
2022-06-13 17:49:45 +0300

    Fixed style

---
+++ src/pages/TrainingPage.styled.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const WrapperNotActiveTrain = styled.div`
+  & .iconPlus {
+    width: 52px;
+    height: 52px;
+    position: absolute;
+    left: 50%;
+    bottom: 0;
+    transform: translateX(-50%);
+    background-color: ${({ theme }) => theme.colors.mainBrandColor};
+    border-radius: 50%;
+    margin: 0 auto;
+  }
+`;
+
+export const WrapperDesktop = styled.div`
+  @media screen and (min-width: ${({ theme }) => theme.breakPoints.desktop}) {
+    display: flex;
+    /* position: relative; */
+  }
+`;
