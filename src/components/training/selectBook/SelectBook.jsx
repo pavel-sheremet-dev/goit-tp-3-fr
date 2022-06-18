1c187ecc233b04212df6bea9606433d46e4449d4
Yulia Novakovskaya <yonovakovskaya@gmail.com>
2022-06-18 21:04:41 +0300

    Fixed training page

--- src/components/training/selectBook/SelectBook.jsx
+++ src/components/training/selectBook/SelectBook.jsx
@@ -4,18 +4,19 @@ import { useTranslation } from 'react-i18next';
 
 import { trainingSelectors } from 'redux/training';
 import { ReactComponent as CalendarIconDowm } from 'images/svg/calendar-icon-down.svg';
+import { statusKeys } from 'helpers/config';
 import { Label } from './SelectBook.styled';
 
 const SelectBook = ({ unreadBooks, getBooksIds, booksIds }) => {
   const { t } = useTranslation();
-  const isActiveTraining = useSelector(trainingSelectors.getStatus);
+  const status = useSelector(trainingSelectors.getStatus);
 
   const filteredBooks = unreadBooks.filter(book => !booksIds.includes(book.id));
 
   return (
     <Label>
       <CalendarIconDowm />
-      {!isActiveTraining ? (
+      {(status !== statusKeys().active) ? (
         <select name="book" onChange={e => getBooksIds(e.target.value)}>
           <option defaultValue="" hidden>
             {t('selectBook')}
